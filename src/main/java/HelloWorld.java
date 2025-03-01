a8fb5e15d48480b0bdac949f592c10f5abbf5071
Petra Senicnjak <petra.senicnjak@gmail.com>
2025-03-01 18:55:39 +0100

    Petra Senicnjak

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel IblMessagge;
+    private JLabel IblMessagge2;
 
     public HelloWorld() {
         setContentPane(panel1);
@@ -12,5 +13,4 @@ public class HelloWorld extends JFrame {
     public static void main(String[] args) {
         new HelloWorld().setVisible(true);
     }
-
 }
