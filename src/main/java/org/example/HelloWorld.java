7b158eb7fc7ef725ef09bd91cd458dcc417a7d0e
Petra Senicnjak <petra.senicnjak@gmail.com>
2025-03-17 16:09:41 +0100

    I added a JLabel

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld extends JFrame {
     private JPanel panel1;
     private JLabel IblMessagge;
     private JLabel IblMessagge2;
+
     //Petra was here now
+    private JLabel IblMessagge3;
 
     public HelloWorld() {
         setContentPane(panel1);
